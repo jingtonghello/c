0c6f9433cb7fd49578c608d98eaa893a97b173ff
zhu_nvim <3406474336@qq.com>
2024-05-09 12:47:22 +0800

    first commit

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream> // 正确的头文件拼写应为 <iostream>
+
+using namespace std; // 正确的命名空间拼写应为 std
+
+#define A 28
+#define B 4.19
+// 符号常量
+// 不需要分号！
+
+int main()
+{
+    cout << A << endl;
+    cout << "nh" << B;
+    return 0;
+}
